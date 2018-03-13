a8e61773f39353f5fe7437ab14b77514298a1cc3
Michael Bailey <michael.bailey3@digital.hmrc.gov.uk>
2018-03-13 15:37:32 +0000

    Added thread.sleeps

--- src/main/scala/Cafe.scala
+++ src/main/scala/Cafe.scala
@@ -1,17 +1,7 @@
-import scala.concurrent.future
-import scala.concurrent.Future
-import scala.util.{Random, Try}
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.util.Random
 import scala.util.control.NonFatal
-
-
-type CoffeeBeans = String
-type GroundCoffee = String
-type Milk  = String
-type FrothedMilk = String
-type Espresso = String
-type Cappuccino = String
 
 case class GrindingException(msg: String) extends Exception(msg)
 
@@ -23,55 +13,76 @@ case class BrewingException(msg: String) extends Exception(msg)
 
 case class Water(temprature: Int)
 
-object Cafe  {
+object Cafe extends App {
 
+  type CoffeeBeans = String
+  type GroundCoffee = String
+  type Milk = String
+  type FrothedMilk = String
+  type Espresso = String
+  type Cappuccino = String
 
-  def grind (beans: CoffeeBeans) : Future[GroundCoffee] = Future {
+  def grind(beans: CoffeeBeans): Future[GroundCoffee] = Future {
     println("start grinding...")
-    Thread.sleep(Random.nextInt(2000))
+//        Thread
+//        .sleep(Random
+//               .nextInt(0))
     if (beans == "baked beans") throw GrindingException("are you joking?")
     println("finished grinding...")
     s"ground coffee of $beans"
   }
 
-  def heatWater(water: Water) : Future[Water] = Future {
+  def heatWater(water: Water): Future[Water] = Future {
     println("heating the water now")
-    Thread.sleep(Random.nextInt(2000))
+//    Thread
+//    .sleep(Random
+//           .nextInt(0))
     println("hot, it's hot!")
-    water.copy(temprature = 85)
+    water
+    .copy(temprature = 85)
   }
 
-  def frothMilk(milk: Milk) : Future[FrothedMilk] = Future {
+  def frothMilk(milk: Milk): Future[FrothedMilk] = Future {
     println("milk frothing system engaged")
-    Thread.sleep(Random.nextInt(2000))
+//    Thread
+//    .sleep(Random
+//           .nextInt(0))
     println("shutting down milk frothing system")
     s"frothed $milk"
   }
 
-  def brew(coffee: GroundCoffee, heatedWater: Water) : Future[Espresso] = Future{
+  def brew(coffee: GroundCoffee, heatedWater: Water): Future[Espresso] = Future {
     println("happy brewing :)")
-    Thread.sleep(Random.nextInt(2000))
+//    Thread
+//    .sleep(Random
+//           .nextInt(0))
     println("It's brewed!")
     "espresso"
   }
 
-  def combine(espresso: Espresso, frothedMilk: FrothedMilk) : Cappuccino = "cappuccino"
+  def combine(espresso: Espresso, frothedMilk: FrothedMilk): Cappuccino = "cappuccino"
 
+  def prepareCappucinoSequentially(): Future[Cappuccino] = {
 
+    val groundCoffee = grind("arabica beans")
+    val heatedWater = heatWater(Water(85))
+    val frothedMilk = frothMilk("milk")
 
-  def prepareCappucinnoSequentially() : Future[Cappuccino] = {
     for {
-      ground <- grind("arabica beans")
-      water <- heatWater(Water(25))
+      ground <- groundCoffee
+      water <- heatedWater
       espresso <- brew(ground, water)
-      foam <- frothMilk("milk")
-    } yield combine(espresso, foam)
+      foam <- frothedMilk
+    } yield {
+      combine(espresso, foam)
+    }
+  }
+  Cafe
+  .prepareCappucinoSequentially map {
+    c =>
+      println(c)
+  } recover {
+    case NonFatal(e) => println(s"Didn't make cappucino $e")
   }
 }
 
-Cafe.prepareCappucinnoSequentially() map {
-  c =>
-    println(c)
-} recover {
-  case NonFatal(e) => println(s"Didn't make cappucino $e")
-}
